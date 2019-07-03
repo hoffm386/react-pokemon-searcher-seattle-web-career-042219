7e2c07aeebde4d7269fdc231f14de06af0e3e08d
hoffm386 <erinrhof@uw.edu>
2019-07-02 17:56:20 -0700

    added index and search functionality

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,43 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+  constructor(props) {
+    super(props);
+    // charmeleon image wasn't working, so we flipped it backwards by default
+    this.state = {
+      flipped: props.pokemon.name === "charmeleon" ? true : false
+    };
+  }
+  findHp = (pokemon) => {
+    // or without passing an arg we could do: const pokemon = this.props.pokemon;
+    return pokemon.stats.find(stat => stat.name === "hp").value
+
+    // Equivalent to:
+    // const stat = pokemon.stats.find(stat => {
+    //  return stat.name === "hp"
+    // }
+    // return stat.value;
+  }
+  handleFlip = () => {
+    this.setState(prevState => {
+      return {flipped: !prevState.flipped}
+    })
+  }
   render() {
+    const pokemon = this.props.pokemon;
     return (
-      <Card>
+      <Card onClick={this.handleFlip}>
         <div>
           <div className="image">
-            <img alt="oh no!" />
+            <img alt="oh no!" src={!this.state.flipped ? pokemon.sprites.front : pokemon.sprites.back}/>
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{pokemon.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {this.findHp(pokemon)} hp
             </span>
           </div>
         </div>
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -5,14 +5,47 @@ import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
 
 class PokemonPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      pokemons: [],
+      filteredPokemons: []
+    };
+  }
+
+  componentDidMount(){
+    fetch("http://localhost:3000/pokemon")
+    .then(res => res.json())
+    .then(res => {
+      this.setState({
+        pokemons: res,
+        filteredPokemons: res
+      })
+    })
+  }
+
+  // we can infer that Search contains some code like:
+  // this.props.onSearchChange(event, props)
+  searchCallback = (e, semanticProps) => {
+    console.log('🤔')
+    const query = semanticProps.value;
+
+    this.setState(prevState => {
+      const filteredPokemons = prevState.pokemons.filter(pokemon => {
+        return pokemon.name.includes(query);
+      });
+      return {filteredPokemons: filteredPokemons};
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search onSearchChange={_.debounce(this.searchCallback, 500)} showNoResults={false} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemons={this.state.filteredPokemons}/>
         <br />
         <PokemonForm />
       </div>
